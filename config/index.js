3be7d68a254cb89c7cd600a5a13517de5eab8029
arndom <aminonimisi2@gmail.com>
2021-02-28 20:37:14 +0100

    Initial commit from gatsby: (https://github.com/arndom/portfolioV2.git)

---
+++ config/index.js
@@ -0,0 +1,99 @@
+module.exports = {
+  //-- SITE SETTINGS -----
+  author: "@nabilalamin",
+  siteTitle: "Nabil Alamin | Portfolio" ,
+  siteShortTitle: "arndom.", // Used as logo text in header, footer, and splash screen
+  siteDescription:
+    "A Portfolio for Nabil Alamin.",
+  siteUrl: "https://arndom.netlify.app/",
+  siteLanguage: "en_US",
+  siteIcon: "content/favicon.png", // Relative to gatsby-config file
+  seoTitleSuffix: "Portfolio", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+
+  // -- THEME SETTINGS -----
+  colors: {
+    lightTheme: {
+      primary: "#333131",
+      secondary: "#FFF4D9",
+      tertiary: "#F2F2F2",
+      text: "#000000",
+      subtext: "#555555",
+      background: "#b8c6db",
+      card: "#abb7c9",
+      scrollBar: "rgba(0, 0, 0, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+    darkTheme: {
+      primary: "#FAFAFA",
+      secondary: "#3d3c38",
+      tertiary: "#252525",
+      text: "rgba(255, 255, 255, 0.87)",
+      subtext: "#AAAAAA",
+      background: "#0D1117",
+      card: "#171e29",
+      scrollBar: "rgba(255, 255, 255, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+  },
+  fonts: {
+    primary: "Roboto, Arial, sans-serif",
+  },
+
+  //-- ARTICLES SECTION SETTINGS -----
+  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+  mediumRssFeed:
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
+  // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
+
+  shownArticles: 3,
+
+  //-- SOCIAL MEDIA SETTINGS -----
+  // There are icons available for the following platforms:
+  // Medium, GitHub, LinkedIn, XING, Behance
+  socialMedia: [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/nabil-alamin/",
+    },
+    {
+      name: "Github",
+      url:
+        "https://github.com/arndom",
+    },
+  ],
+
+  //-- NAVIGATION SETTINGS -----
+  navLinks: {
+    menu: [
+      {
+        name: "About Me",
+        url: "/#about",
+      },
+      {
+        name: "Skills",
+        url: "/#interests",
+      },
+      {
+        name: "Projects",
+        url: "/#projects",
+      },
+    ],
+    button: {
+      name: "Contact",
+      url: "/#contact",
+    },
+  },
+  footerLinks: [
+    {
+      name: "Privacy",
+      url: "/privacy",
+    },
+    {
+      name: "Imprint",
+      url: "/imprint",
+    },
+  ],
+}
